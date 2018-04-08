db47b6e224651a0ccf08bc07309e6c5273441afa
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-08 18:38:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians,instruments){
+ var emptyarray = []
+  for (var i = 0; i<instruments.length;i++){
+    
+    emptyarray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+    return emptyarray
+  }
+  function johnLennonFacts(facts){
+   var i = 0
+    while (i<facts.length){
+      facts[i] += "!!!"
+     i++
+    }
+  return facts
+    
+  }
+  function iLoveTheBeatles(n){
+    var emptyarray = []
+   var counter = 0
+    do {
+      emptyarray.push("I love the Beatles!")
+    }while (n<15){
+      counter++
+    }
+       return emptyarray
+  }
